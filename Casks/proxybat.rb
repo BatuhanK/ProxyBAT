d5a4b29c35987c47dbacc32d5651553825f9fbf3
Batuhan KATIRCI <katircib@gmail.com>
2026-02-26 00:53:00 +0300

    fix: update Homebrew cask with correct SHA256 for v0.2.3

--- Casks/proxybat.rb
+++ Casks/proxybat.rb
@@ -2,8 +2,8 @@ cask 'proxybat' do
   arch arm: 'arm64', intel: 'x64'
 
   version '0.2.3'
-  sha256 arm: 'PLACEHOLDER_ARM64_SHA256',
-         intel: 'PLACEHOLDER_INTEL_SHA256'
+  sha256 arm: 'f50bfd3e249382d4819520fd68992e7b9fb7a6439bf5ac90e5ae5451c41d8b9c',
+         intel: '0a870649eed58bf729068289bd35fa8e576c11fb1afe12369b1609095de4d5a1'
 
   url "https://github.com/batuhank/proxybat/releases/download/v#{version}/ProxyBat-#{version}#{arch == 'arm64' ? '-arm64' : ''}.dmg"
   name 'ProxyBat'
