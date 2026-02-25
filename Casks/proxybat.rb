324273d37de866cec1537aa40447e18aae5a636e
Batuhan KATIRCI <katircib@gmail.com>
2026-02-26 00:16:08 +0300

    feat: add dual-arch support to Homebrew cask for Intel and Apple Silicon

--- Casks/proxybat.rb
+++ Casks/proxybat.rb
@@ -1,11 +1,19 @@
 cask 'proxybat' do
-  version '1.0.0'
-  sha256 '39e417aa536c07a697d35e7bb516b22e6be21287924660ca90b767f29623de6b'
+  arch arm: 'arm64', intel: 'x64'
 
-  url "https://github.com/batuhank/proxybat/releases/download/v#{version}/ProxyBat-#{version}-arm64.dmg"
+  version '0.2.2'
+  sha256 arm: 'eb2722ecbcc6068add74c05d8d91629202dc588bca2ad22d83d80e4a3f379c6e',
+         intel: 'c18b5322693741cf4b68486bbbc393d697bdc04f21b596a217e04087bff6edac'
+
+  url "https://github.com/batuhank/proxybat/releases/download/v#{version}/ProxyBat-#{version}#{arch == 'arm64' ? '-arm64' : ''}.dmg"
   name 'ProxyBat'
   desc 'MITM Proxy with AI Agent Integration'
   homepage 'https://github.com/batuhank/proxybat'
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app 'ProxyBat.app'
 
